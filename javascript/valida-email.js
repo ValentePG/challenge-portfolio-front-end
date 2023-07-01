155df20d7b3f73d36dd6c3a972dcaf10796a1e05
ValentePG <112831085+ValentePG@users.noreply.github.com>
2023-07-01 14:16:33 -0300

    quase

--- javascript/valida-email.js
+++
@@ -1,3 +0,0 @@
-// export default function verificaEmail(){
-//   const email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
-// }
--- javascript/validacao.js
+++ javascript/validacao.js
@@ -1,68 +1,81 @@
-// const camposDoFormulario = document.querySelectorAll('[required]');
+const camposDoFormulario = document.querySelectorAll('[required]');
+const spanErro = document.querySelectorAll('.formulario__mensagem-erro')
 
-// camposDoFormulario.forEach((campo) => {
-//   campo.addEventListener("blur", () => verificaCampo(campo));
+spanErro.forEach(span => {
+  span.style.display = 'none'
+})
 
-//   campo.addEventListener("invalid", evento => evento.preventDefault());
-// })
-
-// function verificaCampo(campo) {
-//   let mensagem = "";
-//   if (campo.value == "") {
-//   } else {
-//     console.log('Deu bom')
-//   }
+camposDoFormulario.forEach((campo) => {
+  campo.addEventListener("blur", () => verificaCampo(campo));
   
-//   tiposDeErro.forEach(erro => {
-//     if (campo.validity[erro]) {
-//       mensagem = mensagens[campo.name][erro];
-//       console.log(mensagem);
-//     }
-//   })
+  campo.addEventListener("invalid", evento => evento.preventDefault());
+})
 
-//   const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+function verificaCampo(campo) {
+  let mensagem = "";
+  
+  tiposDeErro.forEach(erro => {
+    if (campo.validity[erro]) {
+      mensagem = mensagens[campo.name][erro];
+      console.log(mensagem);
+    }
+  })
+  
+  const mensagemErro = campo.parentNode.querySelector('.formulario__mensagem-erro');
+  const validadorDeInput = campo.checkValidity();
+  
+  if (!validadorDeInput) {
+    mensagemErro.textContent = mensagem;
+    mensagemErro.style.display = "block";
+    
+  } else {
+    mensagemErro.textContent = "";
+    mensagemErro.style.display = "none"
+  }
+}
 
-//   const validadorDeInput = campo.checkValidity();
+const tiposDeErro = [
+  'valueMissing',
+  'typeMismatch',
+  'patternMismatch',
+  'tooShort',
+  'customError'
+]
 
-//   console.log(mensagemErro)
-//   console.log(validadorDeInput);
-
-//   if (!validadorDeInput) {
-//     mensagemErro.textContent = mensagem;
-
-//   } else {
-//     mensagemErro.textContent = "";
-//   }
+const mensagens = {
+  nome: {
+    valueMissing: "O campo de nome não pode estar vazio.",
+  },
+  email: {
+    valueMissing: "O campo de e-mail não pode estar vazio.",
+    typeMismatch: "Por favor, preencha um email válido.",
+  },
+  assunto: {
+    valueMissing: "O campo de assunto não pode estar vazio.",
+  },
+  mensagem: {
+      valueMissing: 'O campo de mensagem não pode estar vazio.',
+      customError: "A mensagem digitada não existe."
+    }
+  }
+  
+// export default function verificaEmail(){
+// const email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
 // }
+const formulario = document.querySelector("[data-formulario]");
 
-// const tiposDeErro = [
-//   'valueMissing',
-//   'typeMismatch',
-//   'patternMismatch',
-//   'tooShort',
-//   'customError'
-// ]
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-// const mensagens = {
-//   nome: {
-//       valueMissing: "O campo de nome não pode estar vazio.",
-//       patternMismatch: "Por favor, preencha um nome válido.",
-//       tooShort: "Por favor, preencha um nome válido."
-//   },
-//   email: {
-//       valueMissing: "O campo de e-mail não pode estar vazio.",
-//       typeMismatch: "Por favor, preencha um email válido.",
-//       tooShort: "Por favor, preencha um e-mail válido."
-//   },
-//   assunto: {
-//       valueMissing: "O campo de assunto não pode estar vazio.",
-//       patternMismatch: "Por favor, preencha um assunto válido.",
-//       tooShort: "O campo de assunto não tem caractéres suficientes."
-//   },
-//   mensagem: {
-//       valueMissing: 'O campo de mensagem não pode estar vazio.',
-//       patternMismatch: "Por favor, digite uma mensagem válida.",
-//       customError: "A mensagem digitada não existe.",
-//       tooShort: "O campo de mensagem não tem caractéres suficientes."
-//   }
-// }
+  const listaRespostas = {
+    "nome": e.target.elements["nome"].value,
+    "email": e.target.elements["email"].value,
+    "assunto": e.target.elements["assunto"].value,
+    "mensagem": e.target.elements["mensagem"].value,
+  }
+
+  
+  localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+  
+  alert('mensagem enviada');
+})
